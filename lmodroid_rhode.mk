53275ddf6edef8be04220667c1c1d218a76f0dea
Dhina17 <dhinalogu@gmail.com>
2024-05-16 09:10:15 +0000

    Bringup LMODroid

---
+++ lmodroid_rhode.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device
+$(call inherit-product, device/motorola/rhode/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := lmodroid_rhode
+PRODUCT_DEVICE := rhode
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g52
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+BUILD_FINGERPRINT := "motorola/rhode_g/rhode:13/T2SRS33.72-22-4-2/e55ba8-388ec3:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=rhode_g \
+    PRIVATE_BUILD_DESC="rhode_g-user 13 T2SRS33.72-22-4-2 e55ba8-388ec3 release-keys"
